8fa637f9ac794bf3205cda0d87c3d2ea3f486e87
charvikanna <charvikanna08@gmail.com>
2024-07-26 13:08:10 -0400

    Initial Commit

---
+++ finalProjectC5/finalProjectC5App.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectC5App.swift
+//  finalProjectC5
+//
+//  Created by Charvi Kanna on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectC5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
