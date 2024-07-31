2913bc24862c1ae584d79fe99e01a5a73845c958
charvikanna <charvikanna08@gmail.com>
2024-07-31 14:50:46 -0400

    third commit

--- map.swift
+++ map.swift
@@ -10,9 +10,30 @@ import SwiftUI
 struct map: View {
     var body: some View {
         NavigationStack {
-            Text("map page")
-            NavigationLink(destination: stats()) {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/
+            ZStack {
+                Color(red:158/255, green: 203/255, blue: 208/255).ignoresSafeArea()
+                VStack {
+                    Text("Maternal Mortality Rates Around the World")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .fontWeight(.bold)
+                    Image("canva").resizable(resizingMode: .stretch).frame(width: 370.0, height: 200.0)
+                    Text("Deaths per 100,000 live births in developed nations")
+                        .font(.caption)
+                        .multilineTextAlignment(.trailing)
+                
+                    Text("The US is the most extreme out of all the developed countries.")
+                        .font(.subheadline).padding(20)
+                    Image("img")
+                    
+                    Text("Despite having one of the most advanced healthcare systems, the U.S. significantly surpasses all other developed countries in\nmaternal mortality rates. As of three years ago, in 2021, the maternal mortality rate was 32.9 in the U.S., exceeding all other industrialized nations.")
+                    Text("This is largely due to disparities in healthcare access and quality, particularly among low-income and minority women.")
+                    
+                    
+                    //Next button
+                    NavigationLink(destination: stats()) {
+                        Image("Image").resizable().aspectRatio(contentMode: .fit).frame(width: 37.0, height: 37.0)
+                    }
+                }
             }
         }
     }
--- resources.swift
+++ resources.swift
@@ -11,35 +11,74 @@ struct resources: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                Color(red:135/255, green: 76/255, blue: 98/255).ignoresSafeArea()
+                Color(red:158/255, green: 203/255, blue: 208/255).ignoresSafeArea()
                 
                 VStack {
                     ZStack {
                         Text("Ways to Help")
                             .font(.largeTitle)
                             .fontWeight(.bold)
+                            .foregroundColor(Color.black)
+                            .multilineTextAlignment(.leading)
+                            .lineLimit(nil)
                             .frame(width: 320.0, height: 60.0)
-                            .background(Rectangle().foregroundColor(.white)).cornerRadius(20).padding()
+                           
                     }
-                    Text("Useful Links!")
+                    Text("1. Raise Awareness & Get Educated")
                         .fontWeight(.bold)
                         .foregroundColor(Color.white)
-                        .multilineTextAlignment(.leading).background(Rectangle().foregroundColor(.yellow)).padding()
-                    Text("Insert links here")
-                        .font(.headline)
-                        .fontWeight(.semibold)
-                        .frame(width: 325.0, height: 200.0)
-                        .background(Rectangle().foregroundColor(.white)).padding()
-                    
-                    ZStack {
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 310.0, height: 30.0)
+                        .background(Rectangle().foregroundColor(Color(hue: 0.721, saturation: 0.803, brightness: 0.444))).padding()
                         
-                        NavigationLink(destination: ContentView()) {
-                            Text("Back to Home").position(x:200, y:350)
-                                .background(Rectangle().foregroundColor(.white)).padding()
-                            //Image("Image").resizable().aspectRatio(contentMode:.fit).frame(width: 24.0).position(x:220, y:100)
+                    Link("National Center for Health Statistics", destination: URL(string: "cdc.gov/nchs/data/hestat/maternal-mortality/2022/maternal-mortality-rates-2022.pdf")!)
+                        .foregroundColor(Color(hue: 0.721, saturation: 0.803, brightness: 0.444)).font(.headline)
+                            .fontWeight(.semibold).padding(3.0)
+                    Link("World Health Organization", destination: URL(string: "https://www.who.int/news-room/fact-sheets/detail/maternal-mortality")!)
+                        .foregroundColor(Color(hue: 0.721, saturation: 0.803, brightness: 0.444)).font(.headline).padding(3.0)
+                    Link("Center for Disease Control and Protection", destination: URL(string: "https://www.cdc.gov/nchs/data/hestat/maternal-mortality/2021/maternal-mortality-rates-2021.htm")!)
+                        .foregroundColor(Color(hue: 0.721, saturation: 0.803, brightness: 0.444)).font(.headline).padding(3.0)
+
+                    Text("2. Learn about Ways to Prevent")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 310.0, height: 30.0)
+                        .background(Rectangle().foregroundColor(Color(hue: 0.721, saturation: 0.803, brightness: 0.444))).padding()
+                    Text(" - Improve access to maternal/prenatal care")
+                        .foregroundColor(Color(hue: 0.721, saturation: 0.803, brightness: 0.444)).font(.headline).padding(3)
+                    Text(" - Address health risks ")
+                        .foregroundColor(Color(hue: 0.721, saturation: 0.803, brightness: 0.444)).font(.headline).padding(3)
+                    Text(" - Provide mental & social support")
+                        .foregroundColor(Color(hue: 0.721, saturation: 0.803, brightness: 0.444)).font(.headline).padding(3)
+                    Text(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(hue: 0.721, saturation: 0.803, brightness: 0.444)).padding(3)
+                     //About US
+                    Text("Meet The Creators!")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 180, height: 30.0)
+                        .background(Rectangle().foregroundColor(Color(hue: 0.721, saturation: 0.803, brightness: 0.444))).padding(8)
+                    HStack {
+                        
+                        Image("eden")
+                            .resizable(resizingMode: .stretch).frame(width: 115.0, height: 170.0)
                             
+                        Image("charvi").resizable(resizingMode: .stretch)
+                            .frame(width: 114.0, height: 170.0)
+                        Image("tanvi").resizable(resizingMode: .stretch)
+                            .frame(width: 140.0, height: 170.0)
+                        
+                    }
+                    
+                    NavigationLink(destination: ContentView()) {
+                        Text("Back to Home")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.black)
+                            .padding(6).frame(width: 180, height: 30.0)
                             
-                        }
                     }
                 }
             }
